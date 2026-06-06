38f54c4f7236c02b422c1b6a5c07adc4aadba48c
Massad Raza <massad.raza@hotmail.com>
2026-06-06 01:06:50 -0400

    Soln. for 3Sum

---
+++ 3Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        result = []
+        nums.sort()
+
+        for index, value in enumerate(nums):
+            if index > 0 and value == nums[index - 1]:
+                continue
+            left = index + 1
+            right = len(nums) - 1
+
+            while left < right:
+                threeSum = value + nums[left] + nums[right]
+                if threeSum > 0:
+                    right -= 1
+                elif threeSum < 0:
+                    left += 1
+                else:
+                    result.append([value, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+        
+        return result
+    
