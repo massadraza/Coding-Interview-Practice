43f6712aee450fb23432a4f997cdedd3d94e12b8
Massad Raza <massad.raza@hotmail.com>
2026-07-02 18:31:30 -0400

    Soln. for LC235

---
+++ lowestCommonAncestor.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        ptr = root
+
+        while ptr:
+            if p.val > ptr.val and q.val > ptr.val:
+                ptr = ptr.right
+            elif p.val < ptr.val and q.val < ptr.val:
+                ptr = ptr.left
+            else:
+                return ptr
