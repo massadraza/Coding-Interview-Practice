a8bf690889e6dd1ced2fa08ead4cf5e2e7bbfc81
Massad Raza <massad.raza@hotmail.com>
2026-07-11 21:46:33 -0400

    Soln. for house robber 2

---
+++ houseRobberII.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(
+            self.houseRobberOne(nums[1:]),
+            self.houseRobberOne(nums[:-1])
+        )
+
+    def houseRobberOne(self, nums):
+        rob1 = 0
+        rob2 = 0
+
+        for n in nums:
+            temp = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = temp
+
+        return rob2
